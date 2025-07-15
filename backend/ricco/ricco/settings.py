7cc0c94413934857242d2198168cb7ba488441b4
Mica <micajuarez26@hotmail.com>
2025-07-15 16:23:00 -0300

    setting

--- backend/ricco/ricco/settings.py
+++ backend/ricco/ricco/settings.py
@@ -89,15 +89,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        # 'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
-        'OPTIONS': {
-            'sql_mode': 'traditional',
-        }
+        'PORT': config('DB_PORT'),     
     }
 }
 
